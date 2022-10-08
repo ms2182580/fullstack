7a489305766d7760a46796f770f3626ade3c0b14
Juan Pasten Castillo <juanpastencastillo@gmail.com>
2022-10-08 01:09:49 -0300

    — in ORGUpdateGeolocation, implemented with serverSideProps() calls only:       ✅ Actual location with API       ✅ Cities nearby with two differents API's       ✅ All of API keys are hidden and the majority of the endpoints as well.

--- pages/ORG/[ORGUpdateGeolocation].js
+++ pages/ORG/[ORGUpdateGeolocation].js
@@ -4,17 +4,16 @@ import { useRouter } from "next/router"
 const ORGUpdateGeolocation = (props) => {
   const router = useRouter()
 
+  console.log("props:", props)
+
   useEffect(() => {
     // console.log("props:", props)
-    
-    const {city, country} = props.data
+    // const {city, country} = props.data
     // console.log('city, country:', city, country)
-    const newObj = {city, country}
+    // const newObj = {city, country}
     // console.log('newObj:', JSON.stringify(newObj))
-
-    localStorage.setItem("INCLUSIVE_GEOLOCATION", JSON.stringify(newObj))
-
-    router.push("/ORG")
+    // localStorage.setItem("INCLUSIVE_GEOLOCATION", JSON.stringify(newObj))
+    // router.push("/ORG")
   }, [])
 
   // return (
@@ -26,21 +25,48 @@ const ORGUpdateGeolocation = (props) => {
 
 export default ORGUpdateGeolocation
 
-export async function getServerSideProps() {
-  // try {
-  //   const res = await fetch(`https://ipinfo.io/json?token=${process.env.API_KEY}`)
-  //   const data = await res.json()
-  //   // console.log('data:', data)
-  //   // localStorage.setItem("INCLUSIVE_GEOLOCATION", )
+export async function getServerSideProps() {  
+  /* 50.000 CALLS PER MONTH */
+  // *Get actual city [START]
+  // const res = await fetch(`${process.env.ENDOPOINT_CURRENT_LOCATION}${process.env.API_KEY}`)
+  // const data = await res.json()
+  // *Get actual city [END]
 
-  // } catch (error) {
-  //   console.log("error:", error)
-  //   setErrorCity(`The error is "${error}"`)
+  /* 1.000 CALLS A DAY */
+  // *Cities arround the actual location [START]
+  // const cityToSearch = data.city
+  //   .split(" ")
+  //   .map((x) => x[0].toUpperCase() + x.slice(1))
+  //   .join("_")
+  // console.log("cityToSearch:", cityToSearch)
+
+  // const wikibaseName = await fetch(
+  //   `${process.env.ENDPOINT_WIKI_ID}${cityToSearch}`
+  // )
+  // const wikibaseToJson = await wikibaseName.json()
+  // const wikibaseCode =
+  //   wikibaseToJson.query.pages[Object.keys(wikibaseToJson.query.pages)[0]]
+  //     .pageprops.wikibase_item
+
+  // const options = {
+  //   method: "GET",
+  //   headers: {
+  //     "X-RapidAPI-Key": `${process.env.HEADERS_X_RAPIDAPI_KEY}`,
+  //     "X-RapidAPI-Host": `${process.env.HEADERS_X_RAPIDAPI_HOST}`
+  //   }
   // }
 
-  // const res = await fetch(`https://ipinfo.io/json?token=${process.env.API_KEY}`)
-  // const data = await res.json()
-  // const data = "Work"
+  // let nearbyCities
+  // try {
+  //   const fetchNearbyCities = await fetch(
+  //     `https://wft-geo-db.p.rapidapi.com/v1/geo/cities/${wikibaseCode}/nearbyCities?radius=100`,
+  //     options
+  //   )
+  //   nearbyCities = await fetchNearbyCities.json()
+  // } catch (error) {
+  //   nearbyCities = `has an error, "${error}"`
+  // }
+  // *Cities arround the actual location [END]
 
   return {
     props: {
@@ -53,7 +79,9 @@ export async function getServerSideProps() {
         org: "AS7418 TELEFÓNICA CHILE S.A.",
         postal: "1240000",
         timezone: "America/Santiago"
-      }
+      },
+      nearbyCities:["someObbj"]
     }
   }
 }
+
--- pages/ORG/index.js
+++ pages/ORG/index.js
@@ -1,39 +1,89 @@
 import { useEffect, useState } from "react"
 import { HyperlinkM } from "../../components/ui/hyperlink/HyperlinkFonts"
 import ORGFirstTime from "./[ORGUpdateGeolocation]"
-import {useORG_KeywordsCtx} from "../../context/ORG_Keywords"
+import { useORG_KeywordsCtx } from "../../context/ORG_Keywords"
 
 const ORG = () => {
   const [geolocationLocalStorage, setGeolocationLocalStorage] = useState(null)
-  const {setKeywordsToSearch} = useORG_KeywordsCtx()
+  const { setKeywordsToSearch } = useORG_KeywordsCtx()
 
   useEffect(() => {
-    if(localStorage.getItem("INCLUSIVE_GEOLOCATION") !== null){
+    if (localStorage.getItem("INCLUSIVE_GEOLOCATION") !== null) {
       setGeolocationLocalStorage("Have geolocation")
     } else {
       setGeolocationLocalStorage(null)
     }
-  },[geolocationLocalStorage])
-  
+  }, [geolocationLocalStorage])
+
   const handleKeywords = (e) => {
     setKeywordsToSearch(e.target.value)
   }
-  
+
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition(function (position) {
+      // console.log("Latitude is :", position.coords.latitude);
+      // console.log("Longitude is :", position.coords.longitude);
+
+      // console.log(`http://getnearbycities.geobytes.com/GetNearbyCities?radius=500&latitude=${position.coords.latitude}&longitude=${position.coords.longitude}&limit=500`)
+
+      // const fetchNearbyCity = fetch(`http://getnearbycities.geobytes.com/GetNearbyCities?radius=500&latitude=${position.coords.latitude}&longitude=${position.coords.longitude}&limit=500`)
+      // const toJson = fetchNearbyCity.json()
+      // console.log('toJson:', toJson)
+
+    //   fetch(
+    //     `http://getnearbycities.geobytes.com/GetNearbyCities?radius=500&latitude=${position.coords.latitude}&longitude=${position.coords.longitude}&limit=5`
+    //   )
+    //     .then((x) => x.json())
+    //     .then((x) => console.log(x))
+    //     .catch((err) => console.log(err))
+    // })
+      // fetch(
+      //   `http://gd2.geobytes.com/GetNearbyCities?radius=100&Latitude=${position.coords.latitude}&Longitude=${position.coords.longitude}&limit=5`
+      // )
+      //   .then((x) => x.json())
+      //   .then((x) => console.log(x))
+      //   .catch((err) => console.log(err))
+      
+      
+      
+      // ? This works but... not very well.
+      // const options = {
+      //   method: 'GET',
+      //   headers: {
+      //     'X-RapidAPI-Key': '44afef096cmshe66ed7bc69592b3p18d5acjsn94eb5f04312a',
+      //     'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+      //   }
+      // };
+      
+      // fetch(`https://wft-geo-db.p.rapidapi.com/v1/geo/locations/${position.coords.latitude}${position.coords.longitude}/nearbyCities?radius=100&limit=10`, options)
+      //   .then(response => response.json())
+      //   .then(response => console.log(response))
+      //   .catch(err => console.error(err));
+      
+        
+        
+        
+        
+    })
+  }, [])
 
   return (
     <>
       <h1>ORG Father component</h1>
-      
-      
-      
-      <input type="text" onChange={handleKeywords}/>
+
+      <input type="text" onChange={handleKeywords} />
 
       {geolocationLocalStorage === null ? (
-        <HyperlinkM href={"/ORG/UpdateGeolocation"} name="To next phase (not have geolocation)" />
+        <HyperlinkM
+          href={"/ORG/UpdateGeolocation"}
+          name="To next phase (not have geolocation)"
+        />
       ) : (
         <button>
-          <HyperlinkM href={"/ORGSpeechTherapiests"} name="To next phase (have geolocation)" />
-          
+          <HyperlinkM
+            href={"/ORGSpeechTherapiests"}
+            name="To next phase (have geolocation)"
+          />
         </button>
       )}
     </>
@@ -41,3 +91,5 @@ const ORG = () => {
 }
 
 export default ORG
+
+
