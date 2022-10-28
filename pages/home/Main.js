b1ddc6a26f9418b9ec26ec714b02634d21788ae6
lenac0des <ellenajean@gmail.com>
2022-10-27 23:13:31 -0400

    Week 8 content: pictures and updated text

--- pages/home/Main.js
+++ pages/home/Main.js
@@ -1,18 +1,29 @@
 import React from 'react'
+import Image from 'next/image'
 import styles from './styles/Main.module.css'
-
+import Chat from '../../assets/images/Chat.png'
+import Group from '../../assets/images/Group.png'
+import Guide from '../../assets/images/Guide.png'
+import Puzzle from '../../assets/images/Puzzle.png'
+import Library from '../../assets/images/Library.png'
 
 export default function Main() {
     return (
-        <section className="MainContainer">
+        <section className={styles.MainContainer}>
             <div className="S1">
-                <div className="WhoWrapper">
-                    {/* <img src={Chat} /> */}
-                    <div className="WhoWeAre">
-                        <h2 className="Who">Who We Are</h2>
-                        <p className="WhoText">Inclusive is a non-profit human services organization and social enterprise in NYC that serves individuals with I/DDs (Intellectual and/or Developmental Disabilities). We specialize in providing services to parents of children whose language development has been impeded by severe physical impairments, life-changing medical conditions, traumatic brain injuries, and many other factors.</p>
-                        
-                        <a href="#" className="A1">Learn More</a>  
+            <div className="WhoWrapper">
+                    <Image 
+                    src={Chat} 
+                    alt= 'Two females sitting shaking hands chatting'
+                    // className={styles.Chatty}
+                    />
+                    <div className= {styles.WhoWeAre}>
+                        <h2 className={styles.Who}>Who We Are</h2>
+                        <p className={styles.WhoText}>Inclusive is a nonprofit in NYC that strives to make life for families with kids with intellectual and developmental disabilities a bit easier.  
+
+We want everyone to benefit from our services. We help people find speech, physical, occupational therapy, and other providers and activities for their kids. 
+
+Our guide to services shows families all the resources available to them and advises them on how to overcome the roadblocks they may face. Inclusive has created a comprehensive database of providers so that parents can find the best specialists for their kids.</p> 
                     </div>
                 </div>
             </div>
@@ -20,24 +31,24 @@ export default function Main() {
 
         <section className="S2">
             <div className="SecondContainer">
-                <div className="WhatWrapper">
-                {/* <img src={Land} /> */}
-                    <div className="WhatWeAre">
+                <div className={styles.WhatWrapper}>
+                <Image src={Group} />
+                <div className={styles.WhatWeAre}>
     
-                        <h2 className="What">What We Do</h2>
-                        <p className="WhatText">Outreach, workshops and training, and live phone support are a few ways Inclusive assists those with I/DD.</p>
-                        <a href="#" className="A2">Learn More</a>
+                    <h2 className={styles.What}>What We Do</h2>
+                    <p className={styles.WhatText}>Outreach, workshops and training, and live phone support are a few ways Inclusive assists those with I/DD.</p>
+                        
                     </div>
                 </div>
             </div>
         </section>
 
-        <section className="S3">
+        <section className={styles.S3}>
             <div className="ThirdContainter">
                 <div className="ResWrapper">
                     <div className="Resources">
-                        <h2 className="RTitle">Resources</h2>
-                        <p className="RText">Here are a few resources that you mind find helpful. Have a look around. </p>
+                    <h2 className={styles.RTitle}>Resources</h2>
+                        <p className={styles.RText}>Here are a few ways Inclusive makes the healthcare journey easier. </p>
                     </div>
                 </div>
             </div>
@@ -45,36 +56,36 @@ export default function Main() {
 
         <section className="S4">
             <div className="CardContainer">
-                <div className="CardWrapper">
+                <div className={styles.CardWrapper}>
 
                     <div>
-                        <div className="Card">
-                            {/* <img className="Thumbnail1" src={Landscape} /> */}
-                            <div className="CardContent">
-                                <h3 classname="CardTitle1">Guide to Services</h3>
-                                <p className="CardText1">The Online Resource Guide (ORG) provides you with an easy-to-use directory of speech therapy providers, services, and local resources.</p>
+                        <div className={styles.Card}>
+                            <Image src={Library} />
+                            <div className={styles.CardContent}>
+                                <h3 classname={styles.CardTitle1}>Guide to Services</h3>
+                                <p className={styles.CardText1}>The resource directory is a searchable database  of speech therapy providers, services, and local resources.</p>
                             </div>
                         </div>
                     </div>
                     
 
                     <div>
-                        <div className="Card">
-                            {/* <img className="Thumbnail2" src={City} /> */}
-                            <div className="CardContent">
-                                <h3 classname="CardTitle2">Resource Directory</h3>
-                                <p className="CardText2">The Inclusive navigation tool shows you the entire process, from request to service, and allows you to view each step in detail.</p>
+                        <div className={styles.Card}>
+                            <Image src={Guide} />
+                            <div className={styles.CardContent}>
+                                <h3 classname={styles.CardTitle2}>Resource Directory</h3>
+                                <p className={styles.CardText2}>The guide to services shows the entire journey, from request to service, and describes each step in detail.</p>
                             </div>
                         </div>
                     </div>
 
 
                     <div>
-                        <div className="Card">
-                            {/* <img className="Thumbnail3" src={Nature} /> */}
-                            <div className="CardContent">
-                                <h3 classname="CardTitle3">Online Portal</h3>
-                                <p className="CardText3">Join our online community to access and save resources, read reviews, and connect with local parents and professionals.</p>
+                        <div className={styles.Card}>
+                            <Image src={Puzzle} />
+                            <div className={styles.CardContent}>
+                                <h3 classname={styles.CardTitle3}>Online Portal</h3>
+                                <p className={styles.CardText3}>The guide to services shows the entire journey, from request to service, and describes each step in detail.</p>
                             </div>
                         </div>
                     </div>
@@ -86,12 +97,12 @@ export default function Main() {
             <div className="FourthContainer">
                 <div className="SubWrapper">
                     <div className="Subscribe">
-                        <h2 className="SubTitle">Subscribe to Our Newsletter</h2>
-                        <p className="SubText">Become a part of our community and gain access to helpful resources.</p>
+                        <h2 className={styles.SubTitle}>Subscribe to Our Newsletter</h2>
+                        <p className={styles.SubText}>to learn about updated services and new providers.</p>
 
-                        <label className="EmailText">Enter Email Address </label>
-                        <input className="InputField" type="email" name="email" placeHolder="example@email.com" required/>
-                        <button className="SubButton">Subscribe</button>
+                        <label className={styles.EmailText}>Email</label>
+                        <input className={styles.InputField} type="email" name="email" placeHolder="example@email.com" required/>
+                        <button className={styles.SubButton}>Subscribe</button>
                     </div>
                 </div>
             </div>
