a6faf6ea0e6824a10278f4b0b60fc2976b1a9920
Juan Pasten Castillo <juanpastencastillo@gmail.com>
2022-11-13 18:31:57 -0300

    FIX:   — Little problem to make the npm run build

--- pages/ORG/SpeechTherapists/[SPDetail].js
+++ pages/ORG/SpeechTherapists/[SPDetail].js
@@ -1,7 +1,6 @@
 import { useRouter } from "next/router";
 import { useState } from "react";
 import { useEffect } from "react";
-import { SpeechtherapistComponent } from "../../../components/ORG/speech-therapists/RepeatedInformation";
 import { SpeechtherapistDetail } from "../../../components/ORG/speech-therapists/SpeechtherapistDetail";
 import {useORG_Ctx_IndividualSpeechtherapist} from "../../../context/ORG_Ctx_IndividualSpeechtherapist"
 
