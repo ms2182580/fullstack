cd0d2da98aa5f2fbc47443962e384cc2ec68493d
Juan Pasten Castillo <juanpastencastillo@gmail.com>
2022-12-30 17:15:24 -0300

    DONE:   — Footer, link accepts "Enter" key of keyboard to go to the link

--- components/footer/FooterMainContent.js
+++ components/footer/FooterMainContent.js
@@ -1,10 +1,20 @@
-import { Fragment } from "react"
+import { Fragment, useState } from "react"
 import { H4 } from "../ui/heading_body_text/HeaderFonts"
 import { LinkNoStyle } from "../ui/hyperlink/HyperlinkNoStyles"
 import {  HyperlinkS } from "../ui/hyperlink/HyperlinkFonts"
 import { FooterMainContentWrapper, Link_Footer_NFNYC } from "./styles/FooterMainContentWrapper.js"
+import { useRouter } from "next/router"
 
 export let FooterMainContent = ({ title, dataToShow = [], dataLink }) => {
+  
+  
+  const router = useRouter()
+  
+  const navigate = (key, theRoute) => {
+    if(key.key==="Enter"){
+      router.push(theRoute)
+    }
+  }
 
   return (
     <FooterMainContentWrapper>
@@ -20,8 +30,8 @@ export let FooterMainContent = ({ title, dataToShow = [], dataLink }) => {
 
         return (
           <Fragment key={x.title} >
-              <span tabIndex={0}>
-                <LinkNoStyle  href={x.route}>{x.title}</LinkNoStyle>
+              <span tabIndex={0} onKeyDown={(e)=> navigate(e, x.route)}>
+                <LinkNoStyle href={x.route}>{x.title}</LinkNoStyle>
               </span>
           </Fragment>
         )
