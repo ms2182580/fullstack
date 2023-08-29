0b4f084e6963798ce4df6c607953453cc14ffd8a
Juan Pasten Castillo <juanpastencastillo@gmail.com>
2023-08-29 19:02:33 -0400

    Fix - Error to push to Vercel: was a typo in some wrapper file, from .JS to .js

--- components/ORG/pages/special-education-schools/second-page/ses-special-education-schools/desktop/SES_D_Results_Header.js
+++ components/ORG/pages/special-education-schools/second-page/ses-special-education-schools/desktop/SES_D_Results_Header.js
@@ -2,7 +2,7 @@ import { useRouter } from "next/router.js"
 import { useCheckBreadcrumbs } from "../../../../../../../utils/ORG/useCheckBreadcrumbs.js"
 import { Breadcrumbs_D } from "../../../../../../ui/breadcrumbs/desktop/Breadcrumbs_D.js"
 import { P } from "../../../../../../ui/heading_body_text/DesktopMobileFonts.js"
-import { SES_D_Results_HeaderWrapper } from "./styles/SES_D_Results_HeaderWrapper.JS"
+import { SES_D_Results_HeaderWrapper } from "./styles/SES_D_Results_HeaderWrapper.js"
 
 export const SES_D_Results_Header = () => {
   const { query } = useRouter()
