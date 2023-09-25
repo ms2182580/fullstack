4e8977144b263e2782feeeeb5517f74d4904eba2
Juan Pasten Castillo <juanpastencastillo@gmail.com>
2023-09-25 16:23:22 -0300

    Added - In Home ComingSoonAI the half of the section is styled

--- components/home/desktop/Home_ComingSoonAI_D.js
+++ components/home/desktop/Home_ComingSoonAI_D.js
@@ -1,9 +1,37 @@
-import { Home_ComingSoonAI_DWrapper } from './styles/Home_ComingSoonAI_DWrapper.js'
+import { Home_SecuritySvg } from '../../../assets/Icons/index'
+import { H3 } from "../../ui/heading_body_text/HeaderFonts.js"
+import { Home_ComingSoonAI_DWrapper } from "./styles/Home_ComingSoonAI_DWrapper.js"
 
 export const Home_ComingSoonAI_D = () => {
   return (
     <Home_ComingSoonAI_DWrapper>
-      Home_ComingSoonAI_D
+      <div className="LEFT">
+        <H3>COMING SOON!</H3>
+        <p>
+          Use <span>ChatGPT</span> for the <br />
+          I/DD community
+        </p>
+        <p>Ask anything about I/DD life and care and get answers you need using generative natural language processing, AI.</p>
+        <button>Talk us now</button>
+
+        <div>
+          <span>
+            <Home_SecuritySvg />
+          </span>
+          <div>
+            Secure, efficient and accurate. <span tabIndex={0}>Learn More</span>
+          </div>
+        </div>
+      </div>
+      {/* 
+      //!FH0
+      
+      Create the rigth part
+      */}
+      {/* <div className="RIGHT">
+        IMAGE
+
+      </div> */}
     </Home_ComingSoonAI_DWrapper>
   )
 }
--- components/home/desktop/INDEX_Home.js
+++ components/home/desktop/INDEX_Home.js
@@ -1,4 +1,5 @@
 import { useNavbarHomeHeight } from "../../../context/NavbarHome_Ctx_Height.js"
+import { Home_ComingSoonAI_D } from "./Home_ComingSoonAI_D.js"
 import { Home_GetServicePlan_D } from './Home_GetServicePlan_D.js'
 import { Home_Hero_D } from "./Home_Hero_D.js"
 import { Home_IntroducingAI_D } from "./Home_IntroducingAI_D.js"
@@ -17,10 +18,12 @@ export const INDEX_Home = () => {
             <Home_GetServicePlan_D />
             <Home_OurPlatformCarousal_D />
             <Home_IntroducingAI_D />
+            <Home_ComingSoonAI_D />
+
 
             {/* >
             
-            <Home_ComingSoonAI_D />
+            
             <Home_PlanWith_D />
             <Home_Pricing_D />
             <Home_Survey_D /> */}
