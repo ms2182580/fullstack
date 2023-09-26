f263b4c8affcb0006fe257caf066680ca7c4293b
Juan Pasten Castillo <juanpastencastillo@gmail.com>
2023-09-26 05:41:52 -0300

    Added - In Home Plan With styled

--- components/home/desktop/Home_PlanWith_D.js
+++ components/home/desktop/Home_PlanWith_D.js
@@ -1,9 +1,40 @@
-import { Home_PlanWith_DWrapper } from './styles/Home_PlanWith_DWrapper.js'
+import Image from "next/image"
+import { Home_PlanWithSvg } from "../../../assets/Icons"
+import Home_PlanWith from "../../../assets/images/Home_PlanWith.png"
+import { H3 } from "../../ui/heading_body_text/HeaderFonts"
+import { Home_PlanWith_DWrapper } from "./styles/Home_PlanWith_DWrapper.js"
 
 export const Home_PlanWith_D = () => {
   return (
     <Home_PlanWith_DWrapper>
-       ...
+      <div className="LEFT">
+        <Image
+          src={Home_PlanWith}
+          layout="responsive"
+          objectFit="contain"
+          width={1}
+          height={1}
+          alt="Image of AI Inclusive dashboard"
+        />
+      </div>
+
+      <div className="RIGHT">
+        <H3>COMING SOON!</H3>
+        <p>
+          Plan with <span>alternative communication</span>
+        </p>
+        <p>Make audio plans, translated into 6 languages with AAC accessibility. Listen to recommendations on your Amazon <span>Echo!</span></p>
+        <button>See all integrations</button>
+
+        <div>
+          <span>
+            <Home_PlanWithSvg />
+          </span>
+          <div>
+            Learn more about service plans & privacy. <span tabIndex={0}>Learn More</span>
+          </div>
+        </div>
+      </div>
     </Home_PlanWith_DWrapper>
   )
 }
