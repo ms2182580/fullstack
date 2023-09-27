e50ea7fae5eb6354a676d4f2cd4eb97590c54a5f
Juan Pasten Castillo <juanpastencastillo@gmail.com>
2023-09-26 23:18:13 -0300

    Changed - Footer desktop styled updated

--- components/footer/desktop/Footer_D.js
+++ components/footer/desktop/Footer_D.js
@@ -1,32 +1,42 @@
-import {
-  Facebook3Svg,
-  Footer_D_AccessibilityModeSvg,
-  Instagram2Svg,
-  LanguageIconSvg,
-  LinkedinSvg,
-  Twitter2Svg
-} from "../../../assets/Icons/index.js"
+import { useRouter } from "next/router.js"
+import { ChatAILogo, Facebook3Svg, Footer_D_AccessibilityModeSvg, Instagram2Svg, LanguageIconSvg, LinkedinSvg, Twitter2Svg } from "../../../assets/Icons/index.js"
 import { Caption, P } from "../../../components/ui/heading_body_text/DesktopMobileFonts.js"
-import { useCtx_ShowModal } from "../../../context/Ctx_ShowModal.js"
 import { H2, H4 } from "../../ui/heading_body_text/HeaderFonts.js"
 import { Footer_DWrapper } from "./styles/Footer_DWrapper.js"
 
 export const Footer_D = () => {
-  const { modalShowedCtx } = useCtx_ShowModal()
+  let router = useRouter()
+
+  let moveToORG = (e) => {
+    if (e.type === "click" || e.key === "Enter") {
+      router.push("/ORG")
+    }
+  }
+
+  let moveToTop = (e) => {
+    if (e.type === "click" || e.key === "Enter") {
+      router.push("/")
+    }
+  }
 
   return (
     <Footer_DWrapper>
       <div>
         <div>
           <H4>INCLUSIVE</H4>
-          <p>About us</p>
-          <p>Contact us</p>
+          <p tabIndex={0}>About us</p>
+          <p tabIndex={0}>Contact us</p>
           <p>Submit Feedback </p>
         </div>
 
         <div>
           <H4>Resource directory</H4>
-          <p>Find resources</p>
+          <p
+            onClick={moveToORG}
+            onKeyDown={moveToORG}
+            tabIndex={0}>
+            Find resources
+          </p>
           <p>Write a review</p>
           <p>Add a listing </p>
           <p>Get listed </p>
@@ -48,15 +58,14 @@ export const Footer_D = () => {
       </div>
 
       <div>
-        <button>
+        <button tabIndex={0}>
           {/* 
           !FH!
           Bug here: the avg only render in landing page and 2° page of ORG. Doesn't render on 1° page of ORG or 3° page
           */}
-
           <Footer_D_AccessibilityModeSvg /> <P semibold>Accessibility Mode</P>
         </button>
-        <button>
+        <button tabIndex={0}>
           <P semibold>Try Premium</P>
         </button>
       </div>
@@ -64,11 +73,13 @@ export const Footer_D = () => {
       <div />
 
       <div>
-        <H2
-          bold
-          logo>
-          INCLUSIVE
-        </H2>
+        <div
+          onClick={moveToTop}
+          onKeyDown={moveToTop}
+          tabIndex={0}>
+          <H2>Inclusive</H2>
+          <ChatAILogo />
+        </div>
         <Caption>The public benefit company connecting parents with the I/DD community.</Caption>
       </div>
 
@@ -76,16 +87,14 @@ export const Footer_D = () => {
         <Caption>© 2023 The INCLUSIVE Group, All rights reserved.</Caption>
 
         <span>
-          <div>
+          <div tabIndex={0}>
             <LanguageIconSvg /> <Caption>English (US)</Caption>
           </div>
           <div>
-            <Twitter2Svg /> <Facebook3Svg /> <Instagram2Svg /> <LinkedinSvg />{" "}
+            <Twitter2Svg tabIndex={0} /> <Facebook3Svg tabIndex={0} /> <Instagram2Svg tabIndex={0} /> <LinkedinSvg tabIndex={0} />{" "}
           </div>
         </span>
       </div>
-
-      {/* <InFrontModal_D_Wrapper modalShowedCtx={modalShowedCtx} /> */}
     </Footer_DWrapper>
   )
 }
