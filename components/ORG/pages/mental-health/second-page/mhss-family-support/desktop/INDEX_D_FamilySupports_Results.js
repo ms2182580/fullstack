055aa60d20bb0e460ad92e5662af174a506e8595
Jaime Campusano <jcampusanovalbuena.com>
2023-10-03 17:19:18 -0300

    Added - MentalHealth structure advanced2

--- components/ORG/pages/mental-health/second-page/mhss-family-support/desktop/INDEX_D_FamilySupports_Results.js
+++ components/ORG/pages/mental-health/second-page/mhss-family-support/desktop/INDEX_D_FamilySupports_Results.js
@@ -1,12 +1,28 @@
-import Image from "next/image"
-import UnderConstructionImagePurple from "../../../../../../../assets/images/UnderConstructionImagePurple.png"
-import { INDEX_D_FamilySupports_ResultsWrapper } from './styles/INDEX_D_FamilySupports_ResultsWrapper.js'
+import { useEffect } from "react"
+import { useORG_Ctx_D_SecondpageData } from "../../../../../../../context/ORG_Ctx_D_SecondpageData_Provider.js"
+import { useORG_Ctx_D_SecondpageFilters } from "../../../../../../../context/ORG_Ctx_D_SecondpageFilters_Provider.js"
+import { DATA_PCMPS_D } from "../../../../../../../utils/ORG/pcmps/DATA_PCMPS_D.js"
+import { DATA_DAY_D_CardLeft, DATA_DAY_D_CardRight } from "../../../../../../../utils/ORG/pcmps/day/DATA_DAY_D_Card.js"
+import { renderFiltersInUI } from "../../../../../../../utils/ORG/pcmps/day/DATA_DAY_D_Filters.js"
+import { INDEX_ORG_Search_D } from "../../../../../cards/second-page/desktop/INDEX_ORG_Search_D.js"
 
 export const INDEX_D_FamilySupports_Results = () => {
+  const { secondpageFiltersORG, setSecondpageFiltersORG } = useORG_Ctx_D_SecondpageFilters()
+  const { setSecondpageDataORG } = useORG_Ctx_D_SecondpageData()
+
+  useEffect(() => {
+    setSecondpageFiltersORG(renderFiltersInUI)
+    setSecondpageDataORG({
+      cardData: DATA_PCMPS_D[0].slice(1),
+      mainNameORG: DATA_PCMPS_D[0][0],
+      right: DATA_DAY_D_CardRight,
+      left: DATA_DAY_D_CardLeft,
+    })
+  }, [])
+
   return (
-    <INDEX_D_FamilySupports_ResultsWrapper>
-      <h1>Family Support Services</h1>
-      <Image src={UnderConstructionImagePurple} />
-    </INDEX_D_FamilySupports_ResultsWrapper>
+    <>
+      {secondpageFiltersORG !== "" && <INDEX_ORG_Search_D />}
+    </>
   )
 }
