efb79ac0f8c6449dcf45d63f92c39e1c1eb63a4b
Jaime Campusano <jcampusanovalbuena.com>
2023-10-04 01:34:15 -0300

    Added - MentalHealth filters finished

--- utils/ORG/pmhss/fs/DATA_FS_D_Filters.js
+++ utils/ORG/pmhss/fs/DATA_FS_D_Filters.js
@@ -1,13 +1,11 @@
-import { ORG_Filters_D_Checkbox2 } from "../../../../components/ORG/filters/desktop/ORG_Filters_D_Checkbox2"
-import { ORG_Filters_D_Range2 } from "../../../../components/ORG/filters/desktop/ORG_Filters_D_Range2"
-import { DATA_ORG_KeyNamesForFilters_D } from "../../DATA_ORG_KeyNamesForFilters_D"
+import { ORG_Filters_D_Checkbox2 } from "../../../../components/ORG/filters/desktop/ORG_Filters_D_Checkbox2";
+import { ORG_Filters_D_Checkbox_Isolated } from "../../../../components/ORG/filters/desktop/ORG_Filters_D_Checkbox_Isolated";
+import { ORG_Filters_D_More2 } from "../../../../components/ORG/filters/desktop/ORG_Filters_D_More2";
+import { ORG_Filters_D_Range2 } from "../../../../components/ORG/filters/desktop/ORG_Filters_D_Range2";
+import { ORG_FILTERS_DATA_D, ORG_FILTERS_KEYS_D } from "../../../ORG_FiltersCategories";
+import { DATA_ORG_KeyNamesForFilters_D } from "../../DATA_ORG_KeyNamesForFilters_D";
+import { DATA_MH_D_Filters } from "../mh/DATA_MH_D_Filters";
 
-import { ORG_FILTERS_DATA_D, ORG_FILTERS_KEYS_D } from "../../../../../../../utils/ORG_FiltersCategories.js"
-import { ORG_Filters_D_Checkbox } from "../../../../../filters/desktop/ORG_Filters_D_Checkbox.js"
-import { ORG_Filters_D_Checkbox_Isolated } from "../../../../../filters/desktop/ORG_Filters_D_Checkbox_Isolated.js"
-import { ORG_Filters_D_More } from "../../../../../filters/desktop/ORG_Filters_D_More.js"
-import { ORG_Filters_D_Range } from "../../../../../filters/desktop/ORG_Filters_D_Range.js"
-import { MH_D_Results_FilterListMainWrapper } from "./styles/MH_D_Results_FilterListMainWrapper.js"
 
 export const DATA_FS_D_Filters = {
   price: {
@@ -34,7 +32,14 @@ export const DATA_FS_D_Filters = {
   mobility: {
     buttonName: "Mobility",
     titleOnModal: "Mobility Features",
-    categoriesToDisplay: ["Foldable", "Reclinig Backrests", "Adjustable Armrests", "Adjustable Leg Rests", "Power Elevating Seats", "Lightweight Frames"],
+    categoriesToDisplay: [
+      "Foldable",
+      "Reclinig Backrests",
+      "Adjustable Armrests",
+      "Adjustable Leg Rests",
+      "Power Elevating Seats",
+      "Lightweight Frames",
+    ],
   },
   seat: {
     buttonName: "Seat",
@@ -46,79 +51,7 @@ export const DATA_FS_D_Filters = {
     titleOnModal: "Setting",
     categoriesToDisplay: ["Indoor", "Outdoor"],
   },
-}
-
-
-
-export const renderFiltersInUI_MHSS_FS = [
-  {
-    [DATA_ORG_KeyNamesForFilters_D.COMPONENT]: ORG_Filters_D_Checkbox2,
-    [DATA_ORG_KeyNamesForFilters_D.PARAMETERS]: {
-      buttonName: DATA_AT_D_Filters.price.buttonName,
-      min: DATA_AT_D_Filters.price.min,
-      max: DATA_AT_D_Filters.price.max,
-      labelName: DATA_AT_D_Filters.price.labelName,
-      addCharacter: DATA_AT_D_Filters.price.addCharacter,
-      whichMeasure: DATA_AT_D_Filters.price.whichMeasure,
-    },
-    shouldAddClassName: false,
-
-  },
-  {
-    [DATA_ORG_KeyNamesForFilters_D.COMPONENT]: ORG_Filters_D_Range2,
-    [DATA_ORG_KeyNamesForFilters_D.PARAMETERS]: {
-      buttonName: DATA_AT_D_Filters.weight.buttonName,
-      min: DATA_AT_D_Filters.weight.min,
-      max: DATA_AT_D_Filters.weight.max,
-      labelName: DATA_AT_D_Filters.weight.labelName,
-      addCharacter: DATA_AT_D_Filters.weight.addCharacter,
-      whichMeasure: DATA_AT_D_Filters.weight.whichMeasure,
-    },
-    shouldAddClassName: false,
-  },
-  {
-    [DATA_ORG_KeyNamesForFilters_D.COMPONENT]: ORG_Filters_D_Checkbox2,
-    [DATA_ORG_KeyNamesForFilters_D.PARAMETERS]: {
-      buttonName: DATA_AT_D_Filters.type.buttonName,
-      titleOnModal: DATA_AT_D_Filters.type.titleOnModal,
-      categoriesToDisplay: DATA_AT_D_Filters.type.categoriesToDisplay,
-    },
-    shouldAddClassName: false,
-  },
-  {
-    [DATA_ORG_KeyNamesForFilters_D.COMPONENT]: ORG_Filters_D_Checkbox2,
-    [DATA_ORG_KeyNamesForFilters_D.PARAMETERS]: {
-      buttonName: DATA_AT_D_Filters.mobility.buttonName,
-      titleOnModal: DATA_AT_D_Filters.mobility.titleOnModal,
-      categoriesToDisplay: DATA_AT_D_Filters.mobility.categoriesToDisplay,
-    },
-    shouldAddClassName: false,
-  },
-  {
-    [DATA_ORG_KeyNamesForFilters_D.COMPONENT]: ORG_Filters_D_Checkbox2,
-    [DATA_ORG_KeyNamesForFilters_D.PARAMETERS]: {
-      buttonName: DATA_AT_D_Filters.seat.buttonName,
-      titleOnModal: DATA_AT_D_Filters.seat.titleOnModal,
-      categoriesToDisplay: DATA_AT_D_Filters.seat.categoriesToDisplay,
-    },
-    shouldAddClassName: false,
-  },
-  {
-    [DATA_ORG_KeyNamesForFilters_D.COMPONENT]: ORG_Filters_D_Checkbox2,
-    [DATA_ORG_KeyNamesForFilters_D.PARAMETERS]: {
-      buttonName: DATA_AT_D_Filters.setting.buttonName,
-      titleOnModal: DATA_AT_D_Filters.setting.titleOnModal,
-      categoriesToDisplay: DATA_AT_D_Filters.setting.categoriesToDisplay,
-    },
-    shouldAddClassName: "AA_AT_SETTING_CHECKBOX",
-  },
-]
-
-
-
-
-
-
+};
 
 const allMoreFilters = {
   checkbox: [
@@ -128,7 +61,9 @@ const allMoreFilters = {
     />,
     <ORG_Filters_D_Checkbox_Isolated
       titleOnModal={DATA_MH_D_Filters.moreFilters.outOfPocketFees.buttonName}
-      categoriesToDisplay={DATA_MH_D_Filters.moreFilters.outOfPocketFees.categoriesToDisplay}
+      categoriesToDisplay={
+        DATA_MH_D_Filters.moreFilters.outOfPocketFees.categoriesToDisplay
+      }
     />,
     <ORG_Filters_D_Checkbox_Isolated
       titleOnModal={ORG_FILTERS_KEYS_D.meetingFormat.inMoreFilters}
@@ -150,60 +85,63 @@ const allMoreFilters = {
 
     <ORG_Filters_D_Checkbox_Isolated
       titleOnModal={DATA_MH_D_Filters.moreFilters.providerType.buttonName}
-      categoriesToDisplay={DATA_MH_D_Filters.moreFilters.providerType.categoriesToDisplay}
+      categoriesToDisplay={
+        DATA_MH_D_Filters.moreFilters.providerType.categoriesToDisplay
+      }
     />,
 
     <ORG_Filters_D_Checkbox_Isolated
       titleOnModal={ORG_FILTERS_KEYS_D.wheelChair.titleToShow}
       categoriesToDisplay={ORG_FILTERS_DATA_D.wheelChair}
     />,
-
-
-
-
-
-
-
-
   ],
-}
+};
 
-export const MH_D_Results_FilterListMain = ({ refUserViewShowFullMapFilter }) => {
-  // const { filtersLeftContext: filterData, setFiltersLeftContext: setFilterData } = useORG_Ctx_filtersLeftDesktop()
+export const renderFiltersInUI_MHSS_FS = [
+  {
+    [DATA_ORG_KeyNamesForFilters_D.COMPONENT]: ORG_Filters_D_Checkbox2,
+    [DATA_ORG_KeyNamesForFilters_D.PARAMETERS]: {
+      buttonName: ORG_FILTERS_KEYS_D.diagnosis.titleToShow,
+      categoriesToDisplay: ORG_FILTERS_DATA_D.diagnosis,
+    },
+    [DATA_ORG_KeyNamesForFilters_D.SHOULD_ADD_CLASSNAME]: false,
+  },
+  {
+    [DATA_ORG_KeyNamesForFilters_D.COMPONENT]: ORG_Filters_D_Range2,
+    [DATA_ORG_KeyNamesForFilters_D.PARAMETERS]: {
+      min: ORG_FILTERS_DATA_D.agesServedRange.minimum,
+      max: ORG_FILTERS_DATA_D.agesServedRange.maximum,
+      buttonName: ORG_FILTERS_KEYS_D.agesServedRange.buttonName,
+      addCharacter: ORG_FILTERS_DATA_D.agesServedRange.addCharacter,
+      whichMeasure: ORG_FILTERS_DATA_D.agesServedRange.whichMeasure,
+    },
+    [DATA_ORG_KeyNamesForFilters_D.SHOULD_ADD_CLASSNAME]: false,
+  },
+  {
+    [DATA_ORG_KeyNamesForFilters_D.COMPONENT]: ORG_Filters_D_Checkbox2,
+    [DATA_ORG_KeyNamesForFilters_D.PARAMETERS]: {
+      min: ORG_FILTERS_DATA_D.distanceRange.minimum,
+      max: ORG_FILTERS_DATA_D.distanceRange.maximum,
+      buttonName: ORG_FILTERS_KEYS_D.distanceRange.buttonName,
+      addCharacter: ORG_FILTERS_DATA_D.distanceRange.addCharacter,
+      whichMeasure: ORG_FILTERS_DATA_D.distanceRange.whichMeasure,
+    },
+    [DATA_ORG_KeyNamesForFilters_D.SHOULD_ADD_CLASSNAME]: false,
+  },
+  {
+    [DATA_ORG_KeyNamesForFilters_D.COMPONENT]: ORG_Filters_D_Checkbox2,
+    [DATA_ORG_KeyNamesForFilters_D.PARAMETERS]: {
+      buttonName: DATA_MH_D_Filters.viewOnly.buttonName,
+      categoriesToDisplay: DATA_MH_D_Filters.viewOnly.categoriesToDisplay,
+    },
+    [DATA_ORG_KeyNamesForFilters_D.SHOULD_ADD_CLASSNAME]: false,
+  },
+  {
+    [DATA_ORG_KeyNamesForFilters_D.COMPONENT]: ORG_Filters_D_More2,
+    [DATA_ORG_KeyNamesForFilters_D.PARAMETERS]: {
+      allCheckboxes: allMoreFilters.checkbox,
+    },
+    [DATA_ORG_KeyNamesForFilters_D.SHOULD_ADD_CLASSNAME]: false,
+  },
+];
 
-
-
-  return (
-    <MH_D_Results_FilterListMainWrapper>
-      <ORG_Filters_D_Checkbox
-        buttonName={ORG_FILTERS_KEYS_D.diagnosis.titleToShow}
-        categoriesToDisplay={ORG_FILTERS_DATA_D.diagnosis}
-      />
-
-      <ORG_Filters_D_Range
-        min={ORG_FILTERS_DATA_D.agesServedRange.minimum}
-        max={ORG_FILTERS_DATA_D.agesServedRange.maximum}
-        buttonName={ORG_FILTERS_KEYS_D.agesServedRange.buttonName}
-        addCharacter={ORG_FILTERS_DATA_D.agesServedRange.addCharacter}
-        whichMeasure={ORG_FILTERS_DATA_D.agesServedRange.whichMeasure}
-      />
-
-      <ORG_Filters_D_Range
-        min={ORG_FILTERS_DATA_D.distanceRange.minimum}
-        max={ORG_FILTERS_DATA_D.distanceRange.maximum}
-        buttonName={ORG_FILTERS_KEYS_D.distanceRange.buttonName}
-        addCharacter={ORG_FILTERS_DATA_D.distanceRange.addCharacter}
-        whichMeasure={ORG_FILTERS_DATA_D.distanceRange.whichMeasure}
-      />
-
-
-      <ORG_Filters_D_Checkbox
-        buttonName={DATA_MH_D_Filters.viewOnly.buttonName}
-        categoriesToDisplay={DATA_MH_D_Filters.viewOnly.categoriesToDisplay}
-      />
-
-
-      <ORG_Filters_D_More allCheckboxes={allMoreFilters.checkbox} />
-    </MH_D_Results_FilterListMainWrapper>
-  )
-}
