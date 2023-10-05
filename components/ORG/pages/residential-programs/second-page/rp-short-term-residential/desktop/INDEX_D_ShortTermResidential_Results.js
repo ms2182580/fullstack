75c43a23fd4dcfa584f9cb5e72498373ca970413
Juan Pasten Castillo <juanpastencastillo@gmail.com>
2023-10-05 04:26:51 -0300

    Added - RP SHORT TERM RESIDENTIAL second and third page

--- components/ORG/pages/residential-programs/second-page/rp-short-term-residential/desktop/INDEX_D_ShortTermResidential_Results.js
+++ components/ORG/pages/residential-programs/second-page/rp-short-term-residential/desktop/INDEX_D_ShortTermResidential_Results.js
@@ -1,12 +1,28 @@
-import Image from 'next/image.js'
-import UnderConstructionImagePurple from "../../../../../../../assets/images/UnderConstructionImagePurple.png"
-import { INDEX_D_ShortTermResidential_ResultsWrapper } from './styles/INDEX_D_ShortTermResidential_ResultsWrapper.js'
+import { useEffect } from 'react'
+import { useORG_Ctx_D_SecondpageData } from '../../../../../../../context/ORG_Ctx_D_SecondpageData_Provider'
+import { useORG_Ctx_D_SecondpageFilters } from '../../../../../../../context/ORG_Ctx_D_SecondpageFilters_Provider'
+import { DATA_PLT_D } from '../../../../../../../utils/ORG/prp/DATA_PLT_D'
+import { DATA_LT_D_CardLeft, DATA_LT_D_CardRight } from '../../../../../../../utils/ORG/prp/lt/DATA_LT_D_Card'
+import { renderFiltersInUI_RP_LT } from '../../../../../../../utils/ORG/prp/lt/DATA_LT_D_Filters'
+import { INDEX_ORG_Search_D } from '../../../../../cards/second-page/desktop/INDEX_ORG_Search_D'
 
 export const INDEX_D_ShortTermResidential_Results = () => {
+  const { secondpageFiltersORG, setSecondpageFiltersORG } = useORG_Ctx_D_SecondpageFilters()
+  const { setSecondpageDataORG } = useORG_Ctx_D_SecondpageData()
+
+  useEffect(() => {
+    setSecondpageFiltersORG(renderFiltersInUI_RP_LT)
+    setSecondpageDataORG({
+      cardData: DATA_PLT_D[1].slice(1),
+      mainNameORG: DATA_PLT_D[1][0],
+      right: DATA_LT_D_CardRight,
+      left: DATA_LT_D_CardLeft,
+    })
+  }, [])
+
   return (
-    <INDEX_D_ShortTermResidential_ResultsWrapper>
-      <h1>Short-Term Residential Programs</h1>
-      <Image src={UnderConstructionImagePurple} />
-    </INDEX_D_ShortTermResidential_ResultsWrapper>
+    <>
+      {secondpageFiltersORG !== "" && <INDEX_ORG_Search_D />}
+    </>
   )
 }
