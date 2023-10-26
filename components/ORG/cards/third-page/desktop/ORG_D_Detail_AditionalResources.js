43e150c14ec1f028cc7a29e23a0493f60e08c802
Jaime Campusano <jcampusanovalbuena.com>
2023-10-26 11:52:21 -0300

    Task n°86 feedback

--- components/ORG/cards/third-page/desktop/ORG_D_Detail_AditionalResources.js
+++ components/ORG/cards/third-page/desktop/ORG_D_Detail_AditionalResources.js
@@ -14,6 +14,16 @@ import image2 from "@/assets/images/ORG/attorney-advocates/ORG_AA17.jpg"
 import image3 from "@/assets/images/ORG/attorney-advocates/ORG_AA18.jpg"
 import Image from "next/image.js"
 
+import ArrowImage from "../../../../../assets/Icons/ArrowDown.png"
+
+/*
+ <Image
+            src={ArrowImage} 
+          
+            className="IconoImagen"
+            
+            />
+*/
 
 
 export const ORG_D_Detail_AditionalResources = () => {
@@ -93,7 +103,9 @@ export const ORG_D_Detail_AditionalResources = () => {
          <Image
             src={image2} alt='image2' width={249.6} height={170.4} className="Imagen"
             />
-        </div>        
+        
+           
+        </div>   
          
          <H4 nmedium>Children's advocates NY</H4>
 
--- components/ORG/cards/third-page/desktop/ORG_D_Detail_HelpfulWebResources.js
+++ components/ORG/cards/third-page/desktop/ORG_D_Detail_HelpfulWebResources.js
@@ -6,6 +6,8 @@ import {
   ORG_Detail_SearchUsefulResource,
 } from "../../../../../assets/Icons/index.js";
 
+import ArrowImage from "../../../../../assets/Icons/DownArrowVector.png"
+
 import { ButtonSmall } from "../../../../ui/buttons/general/index.js";
 import { P } from "../../../../ui/heading_body_text/DesktopMobileFonts.js";
 import { H3, H4 } from "../../../../ui/heading_body_text/HeaderFonts.js";
@@ -44,7 +46,7 @@ export const ORG_D_Detail_HelpfulWebResources = () => {
   return (
     <>
       <ORG_D_Detail_HelpfulWebResourcesWrapper ref={toMoveTheView}>
-        <H3 semibold>Helpful web resources</H3>
+        <H3 semibold>Helpful web resources  <button className="DesplegateButton"> <Image src={ArrowImage} layout="fixed"></Image>       </button>      </H3>
 
         <div>
           <ORG_Detail_SearchUsefulResource />
@@ -55,192 +57,211 @@ export const ORG_D_Detail_HelpfulWebResources = () => {
           </span>
         </div>
 
-        <div className="AdvocacyGroupsContainer">
-          <div className="HelpfulWebGroupsItems">
-            <div className="HelpfulWebCard">
-              <div className="ImageContainer">
+       
+
+        <div class="HelpfulWebElementsWrapper">
+
+        <div class="row BottomBorderRow">
+            <div class="column">
+              <div class="ImageColumn">
                 <Image
                   src={image1}
                   alt="image1"
-                  width={340}
-                  height={115}
-                  className="Imagen"
+                  // width={340}
+                  //  height={115}
+                  layout="fill"
+                  objectFit="contain"
+                  //   className="Imagen"
                 />
               </div>
-
-              <div className="WebDescription">
-                <ul>
-                  <li>
-                    <p>
-                      <P
-                        hyperlink_normal
-                        semibold
-                        underline
-                        onClick={null}
-                        onKeyDown={null}
-                        tabIndex={0}
-                      >
-                        <a target="_blank" href="https://www.ablenrc.org/" className="Link">
-                        ABLE National Resource Center
-                        </a>
-                      </P>
-                      Special Needs Alliance and ABLE National Resource Center collaborate on efforts to educate individuals with disabilities, their families and other stakeholders...
-                    </p>
-                  </li>
-                </ul>
-              </div>
-
-              <span onClick={null}>
-                <button className="Button">
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    width="27"
-                    height="26"
-                    viewBox="0 0 27 26"
-                    fill="none"
-                  >
-                    <path
-                      d="M25.7207 1H17.7207M25.7207 1L13.7207 13M25.7207 1V9"
-                      stroke="#6C6C6C"
-                      stroke-width="1.5"
-                      stroke-linecap="round"
-                      stroke-linejoin="round"
-                    />
-                    <path
-                      d="M25.7207 14.3333V22.3333C25.7207 23.0406 25.4398 23.7189 24.9397 24.219C24.4396 24.719 23.7613 25 23.054 25H4.38737C3.68013 25 3.00185 24.719 2.50175 24.219C2.00165 23.7189 1.7207 23.0406 1.7207 22.3333V3.66667C1.7207 2.95942 2.00165 2.28115 2.50175 1.78105C3.00185 1.28095 3.68013 1 4.38737 1H12.3874"
-                      stroke="#6C6C6C"
-                      stroke-width="1.5"
-                      stroke-linecap="round"
-                    />
-                  </svg>
-                  Visit site
-                </button>
-              </span>
             </div>
+            <div class="column">
+              <div class="DescriptionColumn">
+                <div className="WebDescription">
+                  <ul>
+                    <li>
+                      <p>
+                        <a
+                          target="_blank"
+                          href="https://www.ablenrc.org/"
+                          className="Link"
+                        >
+                          ABLE National Resource Center
+                        </a>
+                        Special Needs Alliance and ABLE National Resource Center collaborate on efforts to educate individuals with disabilities, their families and other stakeholders...
+                      </p>
+                    </li>
+                  </ul>
+                </div>
+              </div>
+            </div>
+            <div class="column">
+              <div class="ButtonColumn">
+                <span onClick={null}>
+                  <button className="Button">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      width="27"
+                      height="26"
+                      viewBox="0 0 27 26"
+                      fill="none"
+                    >
+                      <path
+                        d="M25.7207 1H17.7207M25.7207 1L13.7207 13M25.7207 1V9"
+                        stroke="#6C6C6C"
+                        stroke-width="1.5"
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                      />
+                      <path
+                        d="M25.7207 14.3333V22.3333C25.7207 23.0406 25.4398 23.7189 24.9397 24.219C24.4396 24.719 23.7613 25 23.054 25H4.38737C3.68013 25 3.00185 24.719 2.50175 24.219C2.00165 23.7189 1.7207 23.0406 1.7207 22.3333V3.66667C1.7207 2.95942 2.00165 2.28115 2.50175 1.78105C3.00185 1.28095 3.68013 1 4.38737 1H12.3874"
+                        stroke="#6C6C6C"
+                        stroke-width="1.5"
+                        stroke-linecap="round"
+                      />
+                    </svg>
+                    Visit site
+                  </button>
+                </span>
+              </div>
+            </div>
+          </div>
 
-            <div className="HelpfulWebCard">
-              <div className="ImageContainer">
+          <div class="row BottomBorderRow">
+            <div class="column">
+              <div class="ImageColumn">
                 <Image
                   src={image2}
                   alt="image2"
-                  width={340}
-                  height={115}
-                  className="Imagen"
+                  // width={340}
+                  //  height={115}
+                  layout="fill"
+                  objectFit="contain"
+                  //   className="Imagen"
                 />
               </div>
-
-              <div className="WebDescription">
-                <ul>
-                  <li>
-                    <p>
-                      <P
-                        hyperlink_normal
-                        semibold
-                        underline
-                        onClick={null}
-                        onKeyDown={null}
-                        tabIndex={0}
-                      >
-                       <a target="_blank" href="https://www.affordablecollegesonline.org/college-resource-center/resources-for-students-with-disabilities/" className="Link">
-                       Affordable Colleges Online
-                        </a>
-                      </P>
-                      College resources for students with disabilities
-                    </p>
-                  </li>
-                </ul>
-              </div>
-
-              <span onClick={null}>
-                <button className="Button">
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    width="27"
-                    height="26"
-                    viewBox="0 0 27 26"
-                    fill="none"
-                  >
-                    <path
-                      d="M25.7207 1H17.7207M25.7207 1L13.7207 13M25.7207 1V9"
-                      stroke="#6C6C6C"
-                      stroke-width="1.5"
-                      stroke-linecap="round"
-                      stroke-linejoin="round"
-                    />
-                    <path
-                      d="M25.7207 14.3333V22.3333C25.7207 23.0406 25.4398 23.7189 24.9397 24.219C24.4396 24.719 23.7613 25 23.054 25H4.38737C3.68013 25 3.00185 24.719 2.50175 24.219C2.00165 23.7189 1.7207 23.0406 1.7207 22.3333V3.66667C1.7207 2.95942 2.00165 2.28115 2.50175 1.78105C3.00185 1.28095 3.68013 1 4.38737 1H12.3874"
-                      stroke="#6C6C6C"
-                      stroke-width="1.5"
-                      stroke-linecap="round"
-                    />
-                  </svg>
-                  Visit site
-                </button>
-              </span>
             </div>
+            <div class="column">
+              <div class="DescriptionColumn">
+                <div className="WebDescription">
+                  <ul>
+                    <li>
+                      <p>
+                        <a
+                          target="_blank"
+                          href="https://www.affordablecollegesonline.org/college-resource-center/resources-for-students-with-disabilities/"
+                          className="Link"
+                        >
+                          Affordable Colleges Online
+                        </a>
+                        College resources for students with disabilities
+                      </p>
+                    </li>
+                  </ul>
+                </div>
+              </div>
+            </div>
+            <div class="column">
+              <div class="ButtonColumn">
+                <span onClick={null}>
+                  <button className="Button">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      width="27"
+                      height="26"
+                      viewBox="0 0 27 26"
+                      fill="none"
+                    >
+                      <path
+                        d="M25.7207 1H17.7207M25.7207 1L13.7207 13M25.7207 1V9"
+                        stroke="#6C6C6C"
+                        stroke-width="1.5"
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                      />
+                      <path
+                        d="M25.7207 14.3333V22.3333C25.7207 23.0406 25.4398 23.7189 24.9397 24.219C24.4396 24.719 23.7613 25 23.054 25H4.38737C3.68013 25 3.00185 24.719 2.50175 24.219C2.00165 23.7189 1.7207 23.0406 1.7207 22.3333V3.66667C1.7207 2.95942 2.00165 2.28115 2.50175 1.78105C3.00185 1.28095 3.68013 1 4.38737 1H12.3874"
+                        stroke="#6C6C6C"
+                        stroke-width="1.5"
+                        stroke-linecap="round"
+                      />
+                    </svg>
+                    Visit site
+                  </button>
+                </span>
+              </div>
+            </div>
+          </div>
 
-            <div className="HelpfulWebCard">
-              <div className="ImageContainer">
+          <div class="row">
+            <div class="column">
+              <div class="ImageColumn">
                 <Image
                   src={image3}
                   alt="image3"
-                  width={340}
-                  height={115}
-                  className="Imagen"
+                  // width={340}
+                  //  height={115}
+                  layout="fill"
+                  objectFit="contain"
+                  //   className="Imagen"
                 />
               </div>
-
-              <div className="WebDescription">
-                <ul>
-                  <li>
-                    <p>
-                      <P
-                        hyperlink_normal
-                        semibold
-                        underline
-                        onClick={null}
-                        onKeyDown={null}
-                        tabIndex={0}
-                      >
-                        <a target="_blank" href="https://www.ablenrc.org/" className="Link">
-                        The American Association of Settlement Consultants (AASC)
-                        </a>
-                      </P>
-                      The American Association of Settlement Consultants (AASC) is an industry advocacy group committed to a...
-                    </p>
-                  </li>
-                </ul>
-              </div>
-
-              <span onClick={null}>
-                <button className="Button">
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    width="27"
-                    height="26"
-                    viewBox="0 0 27 26"
-                    fill="none"
-                  >
-                    <path
-                      d="M25.7207 1H17.7207M25.7207 1L13.7207 13M25.7207 1V9"
-                      stroke="#6C6C6C"
-                      stroke-width="1.5"
-                      stroke-linecap="round"
-                      stroke-linejoin="round"
-                    />
-                    <path
-                      d="M25.7207 14.3333V22.3333C25.7207 23.0406 25.4398 23.7189 24.9397 24.219C24.4396 24.719 23.7613 25 23.054 25H4.38737C3.68013 25 3.00185 24.719 2.50175 24.219C2.00165 23.7189 1.7207 23.0406 1.7207 22.3333V3.66667C1.7207 2.95942 2.00165 2.28115 2.50175 1.78105C3.00185 1.28095 3.68013 1 4.38737 1H12.3874"
-                      stroke="#6C6C6C"
-                      stroke-width="1.5"
-                      stroke-linecap="round"
-                    />
-                  </svg>
-                  Visit site
-                </button>
-              </span>
             </div>
+            <div class="column">
+              <div class="DescriptionColumn">
+                <div className="WebDescription">
+                  <ul>
+                    <li>
+                      <p>
+                        <a
+                          target="_blank"
+                          href="https://americanasc.org/"
+                          className="Link"
+                        >
+                          The American Association of Settlement Consultants
+                          (AASC)
+                        </a>
+                        The American Association of Settlement Consultants
+                        (AASC) is an industry advocacy group committed to a...
+                      </p>
+                    </li>
+                  </ul>
+                </div>
+              </div>
+            </div>
+            <div class="column">
+              <div class="ButtonColumn">
+                <span onClick={null}>
+                  <button className="Button">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      width="27"
+                      height="26"
+                      viewBox="0 0 27 26"
+                      fill="none"
+                    >
+                      <path
+                        d="M25.7207 1H17.7207M25.7207 1L13.7207 13M25.7207 1V9"
+                        stroke="#6C6C6C"
+                        stroke-width="1.5"
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                      />
+                      <path
+                        d="M25.7207 14.3333V22.3333C25.7207 23.0406 25.4398 23.7189 24.9397 24.219C24.4396 24.719 23.7613 25 23.054 25H4.38737C3.68013 25 3.00185 24.719 2.50175 24.219C2.00165 23.7189 1.7207 23.0406 1.7207 22.3333V3.66667C1.7207 2.95942 2.00165 2.28115 2.50175 1.78105C3.00185 1.28095 3.68013 1 4.38737 1H12.3874"
+                        stroke="#6C6C6C"
+                        stroke-width="1.5"
+                        stroke-linecap="round"
+                      />
+                    </svg>
+                    Visit site
+                  </button>
+                </span>
+              </div>
+            </div>
+          </div>
 
-            <div className="ViewAllLink">
+          <div className="ViewAllLink">
               <P
                 hyperlink_normal
                 semibold
@@ -252,7 +273,7 @@ export const ORG_D_Detail_HelpfulWebResources = () => {
                 View All
               </P>
             </div>
-          </div>
+  
         </div>
       </ORG_D_Detail_HelpfulWebResourcesWrapper>
     </>
