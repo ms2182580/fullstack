82c2013988db236cfc88e737ab98522199cc5492
Jaime Campusano <jcampusanovalbuena.com>
2023-11-02 01:11:19 -0300

    Task 77.4 Specific sections - Admissions

---
+++ components/ORG/cards/third-page/desktop/ORG_D_Detail_Admission.js
@@ -0,0 +1,163 @@
+import { useORG_Ctx_D_ThirdpageData } from "@/context/ORG_Ctx_D_ThirdpageData_Provider.js";
+import { useEffect, useRef, useState } from "react";
+import { ORG_Detail_SearchUsefulResource } from "../../../../../assets/Icons/index.js";
+
+import { ButtonSmall } from "../../../../ui/buttons/general/index";
+import { P } from "../../../../ui/heading_body_text/DesktopMobileFonts.js";
+import { H3, H4 } from "../../../../ui/heading_body_text/HeaderFonts.js";
+
+import { DATA_PAT_D } from "@/utils/ORG/pat/DATA_PAT_D";
+import { ORG_D_Detail_AdditionalResourcesWrapper } from "./styles/ORG_D_Detail_AdditionalResourcesWrapper.js";
+
+import { ORG_D_Detail_AdmissionWrapper } from "./styles/ORG_D_Detail_AdmissionWrapper.js";
+
+import image1 from "@/assets/images/ORG/attorney-advocates/ORG_AA16.jpg";
+import image2 from "@/assets/images/ORG/attorney-advocates/ORG_AA17.jpg";
+import image3 from "@/assets/images/ORG/attorney-advocates/ORG_AA18.jpg";
+import Image from "next/image.js";
+
+import ArrowImage from "../../../../../assets/Icons/ArrowDown.png";
+
+/*
+ <Image
+            src={ArrowImage} 
+          
+            className="IconoImagen"
+            
+            />
+*/
+
+export const ORG_D_Detail_Admission = () => {
+  const { thirdpageDataORG } = useORG_Ctx_D_ThirdpageData();
+
+  const { card } = thirdpageDataORG;
+
+  const [showAll, setShowAll] = useState(false);
+  const toMoveTheView = useRef();
+
+  let isSelected = true;
+
+  const [howMuchDisplay, setHowMuchDisplay] = useState(1);
+
+  useEffect(() => {
+    if (!isSelected) {
+      setHowMuchDisplay(1);
+    } else {
+      setHowMuchDisplay(DATA_PAT_D.length);
+    }
+  }, [isSelected]);
+
+  return (
+    <>
+      <ORG_D_Detail_AdmissionWrapper ref={toMoveTheView}>
+        <header>
+          <H3>Admissions</H3>
+        </header>
+        <section>
+          <H4 primary_hover bold>
+            Application Cycle
+          </H4>
+
+          <P>
+            <ul className="ulNoPoint">
+              <li>
+                <strong>Applications accepted: </strong> September - January
+              </li>
+              <li>
+                <strong>Due date: </strong> January 5th
+              </li>
+              <li>
+                <strong>Financial application due date: </strong>February 10th
+              </li>
+              <li>
+                <strong>Tuition deposit due date: </strong>February 10th
+              </li>
+            </ul>
+          </P>
+        </section>
+
+        <section>
+          <H4 primary_hover bold>
+            Tuition cost
+          </H4>
+
+          <P>
+            <ul className="ulNoPoint">
+              <li>
+                <strong>Base tuition: </strong> $117,232.02
+              </li>
+            </ul>
+          </P>
+          <table>
+            <thead>
+              <tr>
+                <th>Tuition Add-ons</th>
+              </tr>
+              <tbody>
+                <tr>
+                  <td>Speech therapy</td>
+                  <td>$32,000</td>
+                </tr>
+                <tr>
+                  <td>Occupational therapy</td>
+                  <td>$24,000</td>
+                </tr>
+                <tr>
+                  <td>Paraprofessional</td>
+                  <td>$40,000</td>
+                </tr>
+
+                <tr>
+                  <td>Assistive technology</td>
+                  <td>$14,000</td>
+                </tr>
+              </tbody>
+            </thead>
+
+            <div className="ViewAllLink">
+              <P
+                hyperlink_normal
+                semibold
+                underline
+                onClick={null}
+                onKeyDown={null}
+                tabIndex={0}
+              >
+                View All
+              </P>
+            </div>
+          </table>
+        </section>
+
+        <section>
+          <H4 primary_hover bold>
+            Requirements
+          </H4>
+
+          <P>
+            <ul>
+              <li>
+                {
+                  "Children with developmental and learning disabilities must have neuropsychological (< 3 years old, preferred)."
+                }
+              </li>
+              <li>Non-DOE approved school, parents willing to pay </li>
+              <li>Students must have a IEP, 504 Plans not accepted </li>
+            </ul>
+          </P>
+
+          <span onClick={null} className="Button">
+            <ButtonSmall secondary>Apply Now &nbsp;
+            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 16 16" fill="none">
+<path fill-rule="evenodd" clip-rule="evenodd" d="M8 16C12.4183 16 16 12.4183 16 8C16 3.58172 12.4183 0 8 0C3.58172 0 0 3.58172 0 8C0 12.4183 3.58172 16 8 16ZM7.17574 6.53314C7.06321 6.64566 7 6.79827 7 6.9574V10.5574C7 10.7165 7.06321 10.8691 7.17574 10.9817C7.28826 11.0942 7.44087 11.1574 7.6 11.1574C7.75913 11.1574 7.91174 11.0942 8.02426 10.9817C8.13679 10.8691 8.2 10.7165 8.2 10.5574V6.9574C8.2 6.79827 8.13679 6.64566 8.02426 6.53314C7.91174 6.42061 7.75913 6.3574 7.6 6.3574C7.44087 6.3574 7.28826 6.42061 7.17574 6.53314ZM7.17574 4.17574C7.28826 4.06321 7.44087 4 7.6 4C7.75913 4 7.91174 4.06321 8.02426 4.17574C8.13679 4.28826 8.2 4.44087 8.2 4.6C8.2 4.75913 8.13679 4.91174 8.02426 5.02426C7.91174 5.13679 7.75913 5.2 7.6 5.2C7.44087 5.2 7.28826 5.13679 7.17574 5.02426C7.06321 4.91174 7 4.75913 7 4.6C7 4.44087 7.06321 4.28826 7.17574 4.17574Z" fill="#3366CC"/>
+</svg>
+
+
+            
+            </ButtonSmall>
+          </span>
+        </section>
+      </ORG_D_Detail_AdmissionWrapper>
+    </>
+  );
+};
---
+++ components/ORG/cards/third-page/desktop/styles/ORG_D_Detail_AdmissionWrapper.js
@@ -0,0 +1,90 @@
+import styled from "styled-components";
+import { NEUTRALS, PRIMARY } from "../../../../../../assets/Colors";
+import image2 from "@/assets/images/ORG/attorney-advocates/ORG_AA17.jpg";
+
+export const ORG_D_Detail_AdmissionWrapper = styled.div`
+  background: #fcfdfe;
+  box-shadow: 0px 2px 6px rgba(0, 0, 0, 0.2);
+  border-radius: 8px;
+
+  header {
+    padding: 24px;
+    background-color: ${PRIMARY.PRIMARY_BACKGROUND};
+  }
+  //HEADER
+  & > :nth-child(1) {
+  }
+
+  section {
+    display: flex;
+    flex-direction: column;
+    padding-bottom: 38px;
+    padding-top: 24px;
+    border-bottom: 1px solid hsl(0, 0%, 88.6%);
+    margin-left: 24px;
+    margin-right: 24px;
+    padding-left:32px;
+    
+
+    & > :nth-child(1) {
+      // height:50px;
+      margin-bottom: 4px;
+    }
+
+    & > :nth-child(2) {
+      padding-left: 16px;
+    }
+  
+  }  
+
+  .ulNoPoint {
+    list-style-type: none;
+  }
+  li {
+    height: 32px;
+    margin-top: 4px;
+    
+    & > :nth-child(1) {
+      font-weight: 600;
+    }
+  }
+
+  Table{
+font-size:16px;
+border-spacing:0px;
+width:50%;
+padding-top:19px;
+padding-left:16px;
+
+& > :last-child{
+  text-align: right;
+  padding:13px;
+}
+
+  }
+  tr{
+    height:36px;
+   
+    
+  }
+  tr:nth-child(even) {
+      background-color:rgba(211, 214, 215, 0.20);
+      
+    }
+  th{
+    font-size:18px;
+    background-color:rgba(211, 214, 215, 0.20);
+    text-align: left;
+    padding-left:26px;
+  }
+  td{
+    width:100%;
+    padding-left:26px;
+    padding-right:13px;
+  }
+
+  .Button{
+    padding-top:24px;
+  }
+
+`;
