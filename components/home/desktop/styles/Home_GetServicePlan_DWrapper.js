8a3b11ebfe6e9cf39fce3a058ff5cff1aa77b069
Juan Pasten Castillo <juanpastencastillo@gmail.com>
2023-12-20 20:01:11 -0300

    Fix - Layout on home section

--- components/home/desktop/styles/Home_GetServicePlan_DWrapper.js
+++ components/home/desktop/styles/Home_GetServicePlan_DWrapper.js
@@ -1,23 +1,20 @@
 import styled from "styled-components"
 
 export const Home_GetServicePlan_DWrapper = styled.div`
-  margin-inline: clamp(16px, 20vw - 170px, 196px);
+  padding-inline: clamp(16px, 20vw - 170px, 196px);
   & > :nth-child(1) {
     font-size: 64px;
     margin-bottom: 62px;
     text-align: center;
-    
-    letter-spacing: -1.782px; 
+
+    letter-spacing: -1.782px;
   }
-  
-  & > :nth-child(2){
+
+  & > :nth-child(2) {
     display: grid;
     grid-template-columns: repeat(3, 1fr);
     grid-template-rows: 1fr;
     grid-auto-flow: column;
-    gap:44px;
-    
+    gap: 44px;
   }
-  
-  
 `
