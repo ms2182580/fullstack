dfc0b4df2b8b54785b7dbab9b81861389f541729
Juan Pasten Castillo <juanpastencastillo@gmail.com>
2024-05-10 14:43:35 -0400

    Changed - Data on backend is not working as intendeed, rollback made

--- pages/org/index.tsx
+++ pages/org/index.tsx
@@ -1,9 +1,8 @@
 import { LoadingComponent } from "@/components/loading/LoadingComponent"
-import { Data_Context } from "@/context/Ctx_Data"
 import { NamesCategories_KEY } from "@/utils/org/categories/general/ALL_DATA"
+import { trpc } from "@/utils/trpc"
 import { useWidthSize } from "@/utils/useWidthSize"
 import dynamic from "next/dynamic"
-import { useContext } from "react"
 
 const INDEX_D_ORG = dynamic(
   () =>
@@ -24,28 +23,36 @@ const ORG_INDEX = () => {
   const { isMobile } = useWidthSize()
 
   // Get all data
-  const data = useContext(Data_Context)
-  console.log("get all data", { data })
+  // const data = useContext(Data_Context)
+  // console.log("get all data", { data })
 
   // Put all data togheter
+  // const allData = {
+  //   [NamesCategories_KEY["AGENCIES"]]: data.agency.data,
+  //   [NamesCategories_KEY["CAMPS"]]: data.camp.data,
+  //   [NamesCategories_KEY["COMMUNITY INCLUSION & CLASSES"]]: data.classGet.data,
+  //   [NamesCategories_KEY["PHYSICIANS"]]: data.doctor.data,
+  //   [NamesCategories_KEY["SCHOOLS & EDUCATIONAL SERVICES"]]:
+  //     data.education.data,
+  //   [NamesCategories_KEY["ASSISTIVE TECHNOLOGY HARDWARE"]]: data.hardware.data,
+  //   [NamesCategories_KEY["LEGAL RESOURCES"]]: data.legal.data,
+  //   [NamesCategories_KEY["MENTAL HEALTH PROVIDERS & SERVICES"]]:
+  //     data.mentalHealthData.data,
+  //   [NamesCategories_KEY["FOUNDATIONS"]]: data.organization.data,
+  //   [NamesCategories_KEY["OTHER"]]: data.other.data,
+  //   [NamesCategories_KEY["RESIDENTIAL RESOURCES"]]: data.residency.data,
+  //   [NamesCategories_KEY["ASSISTIVE SOFTWARE"]]: data.software.data,
+  //   [NamesCategories_KEY["THERAPEUTIC SERVICES"]]: data.therapy.data,
+  //   [NamesCategories_KEY["VOCATIONAL RESOURCES"]]: data.vocation.data,
+  // }
+
+  const mentalHealthData = trpc.mentalHealth.getAll.useQuery({ limit: 3 })
+
   const allData = {
-    [NamesCategories_KEY["AGENCIES"]]: data.agency.data,
-    [NamesCategories_KEY["CAMPS"]]: data.camp.data,
-    [NamesCategories_KEY["COMMUNITY INCLUSION & CLASSES"]]: data.classGet.data,
-    [NamesCategories_KEY["PHYSICIANS"]]: data.doctor.data,
-    [NamesCategories_KEY["SCHOOLS & EDUCATIONAL SERVICES"]]:
-      data.education.data,
-    [NamesCategories_KEY["ASSISTIVE TECHNOLOGY HARDWARE"]]: data.hardware.data,
-    [NamesCategories_KEY["LEGAL RESOURCES"]]: data.legal.data,
     [NamesCategories_KEY["MENTAL HEALTH PROVIDERS & SERVICES"]]:
-      data.mentalHealthData.data,
-    [NamesCategories_KEY["FOUNDATIONS"]]: data.organization.data,
-    [NamesCategories_KEY["OTHER"]]: data.other.data,
-    [NamesCategories_KEY["RESIDENTIAL RESOURCES"]]: data.residency.data,
-    [NamesCategories_KEY["ASSISTIVE SOFTWARE"]]: data.software.data,
-    [NamesCategories_KEY["THERAPEUTIC SERVICES"]]: data.therapy.data,
-    [NamesCategories_KEY["VOCATIONAL RESOURCES"]]: data.vocation.data,
+      mentalHealthData.data,
   }
+
   /* Check that at least one source is not undefined to put it in the UI */
   const atLeastOneIsNotUndefined = Object.values(allData).some(
     (x) => x !== undefined
