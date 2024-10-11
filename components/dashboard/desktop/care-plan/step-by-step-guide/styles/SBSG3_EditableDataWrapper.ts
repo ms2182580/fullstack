919e1e7849ba85ebfb50b2422c69492620f763f9
Juan Pasten Castillo <juanpastencastillo@gmail.com>
2024-10-11 12:42:12 -0300

    Advances - On dashboard care plan step by step guide 3° component:            Styles of the conente

--- components/dashboard/desktop/care-plan/step-by-step-guide/styles/SBSG3_EditableDataWrapper.ts
+++ components/dashboard/desktop/care-plan/step-by-step-guide/styles/SBSG3_EditableDataWrapper.ts
@@ -1,22 +1,32 @@
+import { NEUTRALS } from "@/assets/Colors"
 import styled from "styled-components"
 
 export const SBSG3_EditableDataWrapper = styled.li`
   display: grid;
-  /* grid-template-columns: repeat(2, 1fr); */
   grid-template-columns: auto auto;
   grid-template-areas:
     "label label"
     "defaultValue icon";
 
+  font-size: 16px;
+  color: ${NEUTRALS.BLACK_2};
+
   & > :nth-child(1) {
     grid-area: label;
+    &::first-letter {
+      text-transform: capitalize;
+    }
   }
 
   & > :nth-child(2) {
     grid-area: defaultValue;
+
     border: none;
 
     transition: background-color 0.3s ease-in-out;
+
+    font-size: inherit;
+    color: ${NEUTRALS.DARK_GREY_3};
 
     &:hover {
       background-color: hsl(208, 100%, 90.1%);
@@ -25,6 +35,11 @@ export const SBSG3_EditableDataWrapper = styled.li`
 
   & > :nth-child(3) {
     grid-area: icon;
-    margin-left: auto;
+
+    & > svg {
+      & > * {
+        stroke: ${NEUTRALS.DARK_GREY_3};
+      }
+    }
   }
 `
