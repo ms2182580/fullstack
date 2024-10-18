a3b6b703cb4eff2cfa20f5679fff91eb6940c7e9
Juan Pasten Castillo <juanpastencastillo@gmail.com>
2024-10-18 20:00:15 -0300

    Changed - On dashboard care plan step by step guide part 4:         * Data is comented to have a better look of the progress [no ci]

--- components/dashboard/desktop/care-plan/step-by-step-guide/SBSG_4.tsx
+++ components/dashboard/desktop/care-plan/step-by-step-guide/SBSG_4.tsx
@@ -1,3 +1,5 @@
+import UnderConstructionImagePurple from "@/assets/images/UnderConstructionImagePurple.png"
+import Image from "next/image"
 import { SBSG_4Wrapper } from "./styles/SBSG_4Wrapper"
 
 /* 
@@ -8,7 +10,7 @@ https://www.figma.com/design/dQTBLDVXlAMPMI5zeAmC4b/Jill-1%2C2%2C3%2C4-(Copy)?no
 export const SBSG_4 = () => {
   return (
     <SBSG_4Wrapper>
-      <header>
+      {/* <header>
         <h5>Services</h5>
       </header>
 
@@ -19,7 +21,12 @@ export const SBSG_4 = () => {
         <li>2</li>
       </ul>
 
-      <p>Search the directory instead</p>
+      <p>Search the directory instead</p> */}
+
+      <Image
+        src={UnderConstructionImagePurple}
+        alt="under construction image"
+      />
     </SBSG_4Wrapper>
   )
 }
--- context/dashboard/care_plan/ctx_create_plan_data_helpers/sbsg3.tsx
+++ context/dashboard/care_plan/ctx_create_plan_data_helpers/sbsg3.tsx
@@ -31,6 +31,14 @@ type SBSG3Types = {
   [NAME_STATES_SBSG3.HANDLER_CHANGE_EDITABLE]: ({ label, defaultValue }) => void
 }
 
+/* 
+!FH0
+- Make the "useHooksSBSG3" function to be able to extract data from the input-tag
+- Make the "ctx-create-plan-data.tsx" for this third part of the component look like the SBSG2 for the names. For that, add the types "SBSG3Types" to the return of the hook "useHooksSBSG3".That should work
+- Check the file "components/dashboard/desktop/care-plan/step-by-step-guide/index.tsx" and check how the props are passed on the variable "SBSG2Props" and check if it keep making sense to have those group of variables (because I'm using a context for that)
+- Keep with SBSG4
+*/
+
 const useHooksSBSG3 = () => {
   const [stateEditableDataSBSG3, setEditableDataSBSG3] =
     useState<StateSBSG3_Type>(SBSG3DefaultList)
