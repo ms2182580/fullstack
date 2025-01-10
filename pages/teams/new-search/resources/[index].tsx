7ca901dbee2d082e8c9e4cdc8924103b64e8137f
Juan Pasten Castillo <juanpastencastillo@gmail.com>
2025-01-10 08:33:50 -0300

    Removed - comments on code

--- pages/teams/new-search/resources/[index].tsx
+++ pages/teams/new-search/resources/[index].tsx
@@ -3,10 +3,3 @@ import { TeamsNewSearchResourcesDynamic } from "@/components/teams/new-search/re
 export default function PAGE_ResourcesSections() {
   return <TeamsNewSearchResourcesDynamic />
 }
-
-/* 
-
-!FH0
-- Crete dynamic here
-- Create component floating bubble of chat is expected to code there
-*/
